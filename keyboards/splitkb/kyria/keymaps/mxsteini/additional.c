0d3b732204ee9532adfc9b0aec0efbd35d80b167
Michael Stein <info@michaelstein-itb.de>
2023-09-14 20:25:19 +0200

    initial commit

---
+++ keyboards/splitkb/kyria/keymaps/mxsteini/additional.c
@@ -0,0 +1,6 @@
+#include QMK_KEYBOARD_H
+
+bool should_process_keypress(void) { return true; }
+
+
+
---
+++ keyboards/splitkb/kyria/keymaps/mxsteini/rules.mk
@@ -0,0 +1,5 @@
+# Although no rules are defined,
+# presence of this file is required for QMK to compile it.
+# Bootloader selection
+# BOOTLOADER = rp2040
+SRC += additional.c
